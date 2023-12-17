70191e50f5ff5dbb6a03fb813c22492279fbf85e
aslan <korpeevaslan@gmail.com>
2023-12-17 16:11:19 +0500

    final fixes

--- src/components/app-button/index.tsx
+++ src/components/app-button/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const AppButton:FC<PropsWithChildren<Props>> = ({children, className, fz, danger = false, size = 'sm', ...props}) => {
   return  <button className={clsx('radius-[10px] rounded-[10px] text-white font-semibold ', {
 		'py-[8px] px-[16px]': size === 'sm',
-		'p-[20px]': size === 'md',
+		'md:p-[20px] p-[10px] text-[14px]': size === 'md',
 		'bg-red-400': danger,
 		'bg-green': !danger,
 		[`text-[${fz}px]`]: fz,
--- src/components/app-select/index.tsx
+++ src/components/app-select/index.tsx
@@ -3,7 +3,10 @@ import Select, {Props} from "react-select";
 export const AppSelect = (props: Props<{value: string; label: string}, true>) => {
 
 
-	return <Select isSearchable={false} styles={{
+	return <Select
+			isSearchable={false}
+			styles={{
+				// @ts-ignore
 		control: (base) => ({...base, padding: "15px", borderRadius: 15})
 	}} {...props}/>
 }
